5245127547adfce100815fb6d80efad7f18279a9
arindammonet <arindam.d@monet.work>
2024-04-16 07:27:22 +0530

    fix: update ui

--- components/about.tsx
+++ components/about.tsx
@@ -5,17 +5,17 @@ const About = () => {
         <h3 className="text-2xl lg:text-6xl font-semibold">About</h3>
 
         <p className="mt-8">
-          We've pioneered the use of decentralized $POINTS on the blockchain,
+          {` We've pioneered the use of decentralized $POINTS on the blockchain,
           leveraging an L2 roll-up chain to establish an interoperable reward
           ecosystem. Our Oracles, powered by smart contracts, bridge the gap
           between traditional retail giants and the dynamic world of
-          Web3-powered loyalty.
+          Web3-powered loyalty.`}
         </p>
         <p className="mt-4">
-          By seamlessly integrating with major retail players, we bring
+          {`By seamlessly integrating with major retail players, we bring
           unparalleled benefits to both businesses and consumers. Our AI-driven
           recommendations ensure that every reward is optimized for maximum
-          utility, tailored to meet the unique needs of each user.
+          utility, tailored to meet the unique needs of each user.`}
         </p>
       </div>
     </section>
--- components/monet-token.tsx
+++ components/monet-token.tsx
@@ -6,17 +6,17 @@ const MonetToken = () => {
 
         <div className="lg:w-1/2">
           <p className="mt-8 break-words">
-            We've pioneered the use of decentralized $POINTS on the blockchain,
+            {`We've pioneered the use of decentralized $POINTS on the blockchain,
             leveraging an L2 roll-up chain to establish an interoperable reward
             ecosystem. Our Oracles, powered by smart contracts, bridge the gap
             between traditional retail giants and the dynamic world of
-            Web3-powered loyalty.
+            Web3-powered loyalty.`}
           </p>
           <p className="mt-4 break-words">
-            By seamlessly integrating with major retail players, we bring
+            {`By seamlessly integrating with major retail players, we bring
             unparalleled benefits to both businesses and consumers. Our
             AI-driven recommendations ensure that every reward is optimized for
-            maximum utility, tailored to meet the unique needs of each user.
+            maximum utility, tailored to meet the unique needs of each user.`}
           </p>
         </div>
       </div>
